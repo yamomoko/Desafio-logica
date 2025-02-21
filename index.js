9d8095b18e1bfd85aa1346eb319f505c32ddc3de
yamomoko <lego_ads@hotmail.com>
2025-02-21 01:39:14 -0300

    adicionei meu nome

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log("ola mundão");
+// para adicionar uma atualização basta 
+// git add. para adicionar tudo q mudou
+// git commit -m "mensagem do que foi feito "para salvar na caixinha 
+// git push -u origin main para ele mandar no repositorio online 
+let name = Alexandre 
