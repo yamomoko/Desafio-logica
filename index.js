9b8816c0620d80acb65b37151357e3c885fbdb4c
yamomoko <lego_ads@hotmail.com>
2025-02-21 01:44:17 -0300

    adicionei mais comentarios

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ console.log("ola mundão");
 // git add. para adicionar tudo q mudou
 // git commit -m "mensagem do que foi feito "para salvar na caixinha 
 // git push -u origin main para ele mandar no repositorio online 
-let name = Alexandre 
+let name = Alexandre;
+// para resgatar projetos so ir no git e copiar o link de clone e add o codigo  git clone e cola o codigo, "cd"e o nome clonado
+//da pra editar online tbm basta aperto o . no github  
